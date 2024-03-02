95180424578cde3256ff343916e4276605838777
Hasan <ha03330224926@gmail.com>
2024-03-02 14:05:48 +0500

    Add webhooks in backend

--- NFT_Gating_Website/backend/server.js
+++ NFT_Gating_Website/backend/server.js
@@ -1,6 +1,11 @@
 import express from "express";
 import memberRoute from "./routes/member.Route.js"
 import cors from "cors";
+import { getRequiredEnvVar, setDefaultEnvVar } from "./envHelpers";
+import {
+  addAlchemyContextToRequest,
+  validateAlchemySignature,
+} from "./webhooksUtil";
 
 const app = express();  // app instance by invoking the express() function. This instance is the foundation of your web-application.
 app.use(cors());
@@ -16,6 +21,43 @@ app.use((req, res, next) => {
 app.use(express.json());   // Server to accept the json data from frontend 
 
 app.use("/api/members", memberRoute);
+
+
+async function main(){
+
+  setDefaultEnvVar("PORT", "8080");
+  setDefaultEnvVar("HOST", "127.0.0.1");
+  setDefaultEnvVar("SIGNING_KEY", "whsec_test");
+
+  const port = +getRequiredEnvVar("PORT");
+  const host = getRequiredEnvVar("HOST");
+  const signingKey = getRequiredEnvVar("SIGNING_KEY");
+
+  // Middleware needed to validate the alchemy signature
+  app.use(
+    express.json({
+      verify: addAlchemyContextToRequest,
+    })
+  );
+  app.use(validateAlchemySignature(signingKey));
+
+  // Register handler for Alchemy Notify webhook events
+  // TODO: update to your own webhook path
+  app.post("/webhook-path", (req, res) => {
+    const webhookEvent = req.body;
+    // Do stuff with with webhook event here!
+    console.log(`Processing webhook event id: ${webhookEvent.id}`);
+    // Be sure to respond with 200 when you successfully process the event
+    res.send("Alchemy Notify is the best!");
+  });
+
+  // Listen to Alchemy Notify webhook events
+  app.listen(port, host, () => {
+    console.log(`Example Alchemy Notify app listening at ${host}:${port}`);
+  });
+}
+
+main();
 
 const PORT = 5000;
 app.listen(PORT, () => {
